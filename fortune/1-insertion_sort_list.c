6f9913287f38ced5895559185357b79d1806bc08
Cipher Phantom <oyepitan10@gmail.com>
2023-07-25 15:00:02 +0100

    Added Files to root

--- fortune/1-insertion_sort_list.c
+++ fortune/1-insertion_sort_list.c
@@ -1,16 +1,26 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm.
+ *
+ * @list: A doubly linked list (pointer to the pointer of the head node)
+ */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *prev, *curr = *list;
+	listint_t *prev, *curr;
 	int swapped;
 
+	if (!list || !*list)
+		return;
+
+	curr = *list;
 	while (curr)
 	{
 		swapped = 0;
 		prev = curr->prev;
-		
+
 		if (prev && curr->n < prev->n)
 		{
 			if (curr->next)
---
+++ fortune/106-bitonic_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * bitonic_sort - Sorts an array of integers in
+ * ascending order using the Bitonic sort algorithm
+ *
+ * @array: An array of integers
+ * @size: The size of the array
+ */
+void bitonic_sort(int *array, size_t size)
+{
+}
