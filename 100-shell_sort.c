23edd8157a955a59ce86ec4e44449a5960f389ec
Arnthorny <chidubem.anthony26@gmail.com>
2023-07-25 18:18:14 -0700

    Adjusted if guard for shell and count sort algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,7 +12,7 @@ void shell_sort(int *array, size_t size)
 	size_t i = 0, j = 0, interval = 1;
 	int temp = 0;
 
-	if (!array || !size)
+	if (!array || size < 2)
 		return;
 
 	while (interval < size / 3)
@@ -22,12 +22,14 @@ void shell_sort(int *array, size_t size)
 	{
 		for (i = interval; i < size; i++)
 		{
-			temp = array[i];
-			for (j = i; j >= interval && (array[j - interval] > temp); j -= interval)
+			for (j = i; j >= interval && (array[j - interval] > array[j]); j -= interval)
+			{
+				temp = array[j];
 				array[j] = array[j - interval];
-			array[j] = temp;
+				array[j - interval] = temp;
+			}
 		}
-		interval = (interval - 1) / 3;
+		interval /= 3;
 		print_array(array, size);
 	}
 }
